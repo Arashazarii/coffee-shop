c824d52268547582b8494ce50e858f6a055b2a61
Arash Azari <arash.azari505@gmail.com>
2026-01-29 01:23:50 +0330

    Navbar and Cartbar added
    Navbar and Cartbar added, some bug fixed.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,18 @@
 const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+const submenuOpenBtn = document.querySelector(".submenu-open-btn");
+const submenu = document.querySelector(".submenu");
+
+const navOpenBtn = document.querySelector(".nav-icon");
+const navCloseBtn = document.querySelector(".nav-close-btn");
+const nav = document.querySelector(".nav");
+
+const cartOpenBtn = document.querySelector(".cart-icon");
+const cartCloseBtn = document.querySelector(".cart-close-btn");
+const cart = document.querySelector(".cart");
+
+const overlay = document.querySelector(".overlay");
+
+// * theme
 toggleThemeBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
     if (localStorage.theme === "dark") {
@@ -10,3 +24,45 @@ toggleThemeBtns.forEach((btn) => {
     }
   });
 });
+
+// * submenu
+submenuOpenBtn.addEventListener("click", (e) => {
+  e.currentTarget.parentElement.classList.toggle("text-orange-300");
+  submenu.classList.toggle("submenu--open");
+});
+
+// * nav
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.remove("-right-64");
+  nav.classList.add("right-0");
+
+  overlay.classList.add("overlay--visible");
+  overlay.addEventListener("click", closeNav);
+});
+
+function closeNav() {
+  nav.classList.remove("right-0");
+  nav.classList.add("-right-64");
+
+  overlay.classList.remove("overlay--visible");
+}
+
+navCloseBtn.addEventListener("click", closeNav);
+
+// * cart
+cartOpenBtn.addEventListener("click", () => {
+  cart.classList.remove("-left-64");
+  cart.classList.add("left-0");
+
+  overlay.classList.add("overlay--visible");
+  overlay.addEventListener("click", closeCart);
+});
+
+function closeCart() {
+  cart.classList.remove("left-0");
+  cart.classList.add("-left-64");
+
+  overlay.classList.remove("overlay--visible");
+}
+
+cartCloseBtn.addEventListener("click", closeCart);
